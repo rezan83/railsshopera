eee76948f74bfa6cc5141f7c1eb8a2b98865941b
rezan83 <rezan.moh83@gmail.com>
2018-06-19 17:44:31 +0000

    4.3 adding new controllers for featured and orders

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -5,4 +5,7 @@ class SimplePagesController < ApplicationController
   end
   def contact
   end
+  def landing_page
+    @featured = Product.first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
 
   get 'simple_pages/index'
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
   get 'simple_pages/about'
   get 'simple_pages/contact'
 
