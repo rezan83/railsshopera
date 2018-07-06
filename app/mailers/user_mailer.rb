9a205c80fbd14bdcbe6ed166b7a707a094ede8e4
rezan83 <rezan.moh83@gmail.com>
2018-07-06 04:44:35 +0000

    5.3 fixing2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,9 @@
 class UserMailer < ApplicationMailer
     default from: 'fakehobo83@gmail.com'
-    @me = 'rezan.moh83@gmail.com'
     def contact_form(email, name, message)
         @message = message
         mail(from: email,
-        to: @me,
+        to: 'rezan.moh83@gmail.com',
         subject: "A new contact form message from #{name}")
     end
 end
