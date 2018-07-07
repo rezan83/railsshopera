3af5d0e509ce5d71fd0baf42a6929fed59bb5a15
rezan83 <rezan.moh83@gmail.com>
2018-07-07 21:13:34 +0000

    5.5 authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
+    rescue_from CanCan::AccessDenied do |exception|
+        exception.default_message = "Not authorized to edit other users"
+        redirect_to main_app.root_url, alert: exception.message
+    end
 end
