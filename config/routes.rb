c09bd7bf05cf86633f598658b39159f02e4b467e
rezan83 <rezan.moh83@gmail.com>
2018-07-08 20:03:53 +0000

    5.5 add current user to nav and edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', edit_user_registration: "edit_user" }
   resources :users
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
   get 'simple_pages/index'
+  get '/user.first_name' => 'users#show'
   root 'products#index'
   get '/featured', to: 'simple_pages#landing_page'
   get 'simple_pages/contact'
