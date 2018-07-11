d5604e6d202d709d7e76268e759ad7ffbd199a6f
rezan83 <rezan.moh83@gmail.com>
2018-07-11 21:51:21 +0000

    5.10 set admin and constrain privilages and access

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,8 +15,12 @@ class CommentsController < ApplicationController
         end
     end
     def destroy
+        @comment = Comment.find(params[:id])
+        product = @comment.product
+        @comment.destroy
+        redirect_to product
     end
-    
+
     private
     def comment_params
         params.require(:comment).permit(:user_id, :body, :rating)
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,15 @@ class Ability
   def initialize(user)
     
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    can :read, :all
+    
+    if user.present?
+      if user.admin?
+        can :manage, :all
+      else
+        can :manage, User, id: user.id
+      end
+    end
   end
 end
     # Define abilities for the passed in user here. For example:
