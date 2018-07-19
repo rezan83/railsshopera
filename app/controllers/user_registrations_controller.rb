8b4d927aa8f4f17b59d055e01d31e0968fe3ae1b
rezan83 <rezan.moh83@gmail.com>
2018-07-19 18:35:51 +0000

    5.11 fixing welcome registration mail 2

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -1,6 +1,7 @@
 class UserRegistrationsController < Devise::RegistrationsController
     def create
         super
+        @user = resource
         if @user.persisted?
             UserMailer.welcome(@user).deliver_now
         end
