4595adaf2defcdfaf6d4f3fb943fb3f01e54f095
rezan83 <rezan.moh83@gmail.com>
2018-07-21 17:05:56 +0000

    6.2 adding product model test

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Product do
+    
+    context "avarage rating" do
+        let (:product) {Product.create!(name: "rog bike")}
+        let (:user) {User.create!(email: "someone@email.com", password: "someone")}
+        before do
+            product.comments.create!(rating: 1, user: user, body: "Awful bike!")
+            product.comments.create!(rating: 3, user: user, body: "Ok bike!")
+            product.comments.create!(rating: 5, user: user, body: "Great bike!")
+        end
+        it "avarage rating should be 3"do
+          expect(product.average_rating).to eq 3
+        end
+        it "unnamed product should be unvalid"do
+          expect(Product.new(description: "not that bad")).not_to be_valid
+        end
+    end
+    
+end
