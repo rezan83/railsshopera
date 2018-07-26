375234cf4eecaa458b0afa1571948c053ce11eee
rezan83 <rezan.moh83@gmail.com>
2018-07-26 20:24:12 +0000

    6.6 creating payments and linking stripe

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get '/featured', to: 'simple_pages#landing_page'
   get 'simple_pages/contact'
   post 'simple_pages/thank_you'
+  post "/payments/create"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
